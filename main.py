11fd5dcd085edc4e210cbbb8a17644ce025bfd34
Juan-Sebastian-Rojas-Sanchez <186082423+Juan-Sebastian-Rojas-Sanchez@users.noreply.github.com>
2024-11-12 00:41:13 -0500

    ejercice 4

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
-num1 = float(input("Ingresa el primer número: "))
-num2 = float(input("Ingresa el segundo número: "))
-operacion = input("Ingresa la operación (+, -, *, /): ")
+lado1 = float(input("Ingresa el primer lado del triángulo: "))
+lado2 = float(input("Ingresa el segundo lado del triángulo: "))
+lado3 = float(input("Ingresa el tercer lado del triángulo: "))
 
-match operacion:
-    case '+':
-        print("Resultado:", num1 + num2)
-    case '-':
-        print("Resultado:", num1 - num2)
-    case '*':
-        print("Resultado:", num1 * num2)
-    case '/':
-        if num2 != 0:
-            print("Resultado:", num1 / num2)
-        else:
-            print("No se puede dividir por cero.")
-    case _:
-        print("Operación no válida.")
+if lado1 == lado2 == lado3:
+    print("El triángulo es equilátero.")
+elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
+    print("El triángulo es isósceles.")
+else:
+    print("El triángulo es escaleno.")
