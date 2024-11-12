0759796cd1e59e17dfb5f59e89d01c74094e569e
Juan-Sebastian-Rojas-Sanchez <186082423+Juan-Sebastian-Rojas-Sanchez@users.noreply.github.com>
2024-11-12 00:34:16 -0500

    ejercice 3

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-nota = int(input("Ingresa una calificación: "))
-if nota >= 60:
-    print("Aprobado")
-else:
-    print("Reprobado")
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+operacion = input("Ingresa la operación (+, -, *, /): ")
+
+match operacion:
+    case '+':
+        print("Resultado:", num1 + num2)
+    case '-':
+        print("Resultado:", num1 - num2)
+    case '*':
+        print("Resultado:", num1 * num2)
+    case '/':
+        if num2 != 0:
+            print("Resultado:", num1 / num2)
+        else:
+            print("No se puede dividir por cero.")
+    case _:
+        print("Operación no válida.")
