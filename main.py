79803fa54f6f7f02dff606f71998cdd293bd5be2
Juan-Sebastian-Rojas-Sanchez <186082423+Juan-Sebastian-Rojas-Sanchez@users.noreply.github.com>
2024-11-12 01:03:51 -0500

    ejercice 7

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import random
-numero_secreto = random.randint(1, 10)
-intento = int(input("Adivina el número (entre 1 y 10): "))
-
-if intento == numero_secreto:
-    print("¡Correcto!")
-elif intento < numero_secreto:
-    print("El número es mayor.")
+num = int(input("Ingresa un número: "))
+if num > 0:
+    print("El número es positivo.")
+elif num < 0:
+    print("El número es negativo.")
 else:
-    print("El número es menor.")
+    print("El número es cero.")
