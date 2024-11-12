777a746c8ddf8983cfe400c36bc43ccbc448e532
Juan-Sebastian-Rojas-Sanchez <186082423+Juan-Sebastian-Rojas-Sanchez@users.noreply.github.com>
2024-11-12 00:33:01 -0500

    ejercice1

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-# Solicitar al usuario un número entero positivo
-n = int(input("Introduce un número entero positivo: "))  # El usuario ingresa un número
-
-# Inicializamos una variable para  factorial
-factorial = 1  # Comenzamos con 1
-
-# Utilizamos un ciclo 'for' para sumar los primeros n números enteros
-for i in range(1, n + 1):  # El rango va de 1 a n (inclusive)
-    factorial = factorial*i  # calculamos factorial
-
-# Mostrar el resultado
-print(f"La suma de los primeros {n} números enteros es: {factorial}")
+num = int(input("Ingresa un número: "))
+if num % 2 == 0:
+    print("El número es par.")
+else:
+    print("El número es impar.")
